312f5bc4de5ae1506fda0c03581d57082a73dd67
curbalman <200753411+curbalman@users.noreply.github.com>
2025-08-18 17:02:53 +0800

    Ex 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,48 @@
 import { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.onClick}> {props.text} </button>
+)
+
+const Feedback = ({handleGoodClick, handleNeutralClick, handleBadClick}) => (
+  <>
+    <h1>give feedback</h1>
+    <Button onClick={handleGoodClick} text="good" />
+    <Button onClick={handleNeutralClick} text="neutral" />
+    <Button onClick={handleBadClick} text="bad" />
+  </>
+)
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = ( good * 1 + bad * (-1) ) / all
+  const positive = ( good / all ) * 100
+  return (
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>{positive} %</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <Feedback handleGoodClick={handleGoodClick} handleNeutralClick={handleNeutralClick} handleBadClick={handleBadClick}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
